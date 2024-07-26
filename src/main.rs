c0ffeec5d184f065a80dd12813c4653109f852c7
Mark <mverleg.noreply@gmail.com>
2024-07-26 23:54:15 +0200

    Follow guide

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn greet() {
+    println!("hello greeting")
+}
+
 fn main() {
-    println!("Hello, world!");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }

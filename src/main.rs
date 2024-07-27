c0ffeea26edab7f43642b9fbdaa7def312f54f4a
Mark <mverleg.noreply@gmail.com>
2024-07-27 12:34:54 +0200

    Buildfile

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn greet() {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        //.invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
